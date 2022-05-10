936c9ae4121ce1a8aebf202463ace449fb944e50
SashBFa <bastienfajs@gmail.com>
2022-05-10 16:50:23 +0200

    user control v1

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const userRoutes = require("./routes/user.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//ROUTES
+app.use("/api/user", userRoutes);
+
+//SERVER
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
