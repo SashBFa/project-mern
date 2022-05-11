ea557f4e4487199e1a7fe79279c2514ba87b22ec
SashBFa <bastienfajs@gmail.com>
2022-05-11 17:02:42 +0200

    user control token

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//jwt
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 
 //ROUTES
 app.use("/api/user", userRoutes);
