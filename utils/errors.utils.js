2da04a9eb12d086e3844828edf0f88d612b238c1
SashBFa <bastienfajs@gmail.com>
2022-05-16 15:52:06 +0200

    backend v1

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -22,3 +22,12 @@ module.exports.signInErrors = (err) => {
     errors.password = "Le mot de passe ne correspond pas";
   return errors;
 };
+
+module.exports.uploadErrors = (err) => {
+  let errors = { format: "", maxSize: "" };
+  if (err.message.includes("invalid file"))
+    errors.format = "Format incompatible";
+  if (err.message.includes("max size"))
+    errors.maxSize = "Le fichier dépasse 500ko";
+  return errors;
+};
